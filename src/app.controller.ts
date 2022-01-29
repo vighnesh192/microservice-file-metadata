8f3f0c9bcce3449ab987311232ce39f2d83e239e
vighnesh192 <vighnesh192000@gmail.com>
2022-01-29 23:27:24 +0530

    Feat: Returning name, type and size of the file

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,16 @@
-import { Controller, Get, Redirect } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Redirect,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { Express } from 'express';
+import { FileInterceptor } from '@nestjs/platform-express';
 
-@Controller()
+@Controller('api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
@@ -9,5 +18,12 @@ export class AppController {
   @Redirect('index.html')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('fileanalyse')
+  @UseInterceptors(FileInterceptor('upfile'))
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    console.log(file);
+    return { name: file.originalname, type: file.mimetype, size: file.size };
   }
 }
